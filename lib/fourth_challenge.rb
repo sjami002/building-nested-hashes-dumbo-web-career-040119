6c59602acf07e80cfafc422cf74e6137dc8c26f5
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-15 20:03:11 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,6 +6,8 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
+        {:name => "", :age => "", :attitude => ""},
+        {:name => "", :age => "", :attitude => ""}
       ]
    }, 
    :capulet => {
@@ -14,6 +16,8 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        {:name => "", :age => "", :attitude => ""},
+        {:name => "", :age => "", :attitude => ""}
       ]
    }
 }
