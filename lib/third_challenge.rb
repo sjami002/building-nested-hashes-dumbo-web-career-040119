bbf356b32e3189067241584e31ae9375af4cf7db
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-15 20:00:15 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,25 +4,36 @@ def third_challenge
       :patriarch => {
         #your key/value pairs here
         :name => "Lord Montague",
-        :age => "54"
+        :age => "53"
         },
       :matriarch => {
         #your key/value pairs here
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
         #your key/value pairs here
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
         },
       :hero_friends => []
    },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
+        :name => "Lord Capulet",
+        :age => "50"
         },
       :matriarch => {
         #your key/value pairs here
+        :name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
         #your key/value pairs here
+        :name => "",
+        :age => ""
         },
       :heroine_friends => []
    }
