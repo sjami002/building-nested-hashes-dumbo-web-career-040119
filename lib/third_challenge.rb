2a2e85ec604d8c79b17863171155577f79dc8bf3
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-15 20:00:34 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -32,8 +32,9 @@ def third_challenge
         },
       :heroine => {
         #your key/value pairs here
-        :name => "",
-        :age => ""
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         },
       :heroine_friends => []
    }
